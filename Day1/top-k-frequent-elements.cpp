c3e12b7df9f83d4903908bf683a162bed09b5863
Sanjeev Yadav <sanjeevyadavcr7@gmail.com>
2026-03-29 20:21:20 +0530

    code: day1

--- Day1/top-k-frequent-elements.cpp
+++ Day1/top-k-frequent-elements.cpp
@@ -6,25 +6,50 @@
 using namespace std;
 using customPair = pair<int, int>;
 
+
 /*
-Time: O(N + MLogM) | Space: O(N)
+Time: O(N) | Space: O(N)
 */
 vector<int> topKFrequent(vector<int>& nums, int k) {
+    int cnt = k;
+    int n = nums.size();
     unordered_map<int, int> freq_map;
-    vector<customPair> freq_nums;
+    vector<vector<int>> nums_bucket(n+1);
     vector<int> top_k_freq_nums;
 
-    for(int& num : nums) freq_map[num]++; // O(N)
-    for(auto& i: freq_map) freq_nums.push_back({i.second, i.first}); // O(M) => M: unique elements
+    for(int& num : nums) freq_map[num]++;
+    for(auto& pair: freq_map) {
+        nums_bucket[pair.second].push_back(pair.first);
+    }
     
-    sort(freq_nums.begin(), freq_nums.end()); // O(M.LogM)
-    
-    int n = freq_nums.size();
-    for(int i=n-1; i>n-1-k; i--) {
-        top_k_freq_nums.push_back(freq_nums[i].second);
+    for(int i=n; i>=0; i--) {
+        for(int num: nums_bucket[i]) {
+            top_k_freq_nums.push_back(num);
+            cnt--;
+            if(cnt == 0) return top_k_freq_nums;
+        }
     }
     return top_k_freq_nums;
 }
+
+/*
+Time: O(N + MLogM) | Space: O(N)
+*/
+// vector<int> topKFrequent(vector<int>& nums, int k) {
+//     unordered_map<int, int> freq_map;
+//     vector<customPair> freq_nums;
+//     vector<int> top_k_freq_nums;
+
+//     for(int& num : nums) freq_map[num]++; // O(N)
+//     for(auto& i: freq_map) freq_nums.push_back({i.second, i.first}); // O(M) => M: unique elements
+//     sort(freq_nums.begin(), freq_nums.end()); // O(M.LogM)
+
+//     int n = freq_nums.size();
+//     for(int i=n-1; i>n-1-k; i--) {
+//         top_k_freq_nums.push_back(freq_nums[i].second);
+//     }
+//     return top_k_freq_nums;
+// }
 
 int main() {
     int n, k;

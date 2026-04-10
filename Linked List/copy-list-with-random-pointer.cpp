f5787992b1240d29f28eeb04e42fadad76c49e40
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-04-11 00:17:15 +0530

    Update copy-list-with-random-pointer.cpp

--- Linked List/copy-list-with-random-pointer.cpp
+++ Linked List/copy-list-with-random-pointer.cpp
@@ -28,3 +28,41 @@ Node* copyRandomList(Node* head) {
 Approach 2: Add cloned pointer in original list. No EXTRA space needed
 Time: O(N) :: Space: O(1)
 */
+Node* copyRandomList(Node* head) {
+    if(!head) return head;
+
+    Node* curr = head;
+
+    // Adding new cloned pointer
+    while(curr) {
+        Node* new_node = new Node(curr->val);
+        new_node->next = curr->next;
+        curr->next = new_node;
+        curr = new_node->next;
+    }
+
+    // Attaching random pointer to cloned nodes
+    curr = head;
+    while(curr) {
+        curr->next->random = curr->random ? curr->random->next : NULL;
+        curr = curr->next->next;
+    }
+
+    // Attaching next pointer to cloned nodes
+    curr = head;
+    Node dummy(-1);
+    Node* copyCurr = &dummy;
+    while(curr) {
+        Node* copy = curr->next;
+
+        // Restore old list
+        curr->next = copy->next;
+        curr = curr->next;
+
+        // Restore new list
+        copyCurr->next = copy;
+        copyCurr = copy;
+    }
+   
+    return dummy.next;
+}

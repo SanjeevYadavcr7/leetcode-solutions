f29fc98979cab6d5da0922997d3cb14ac2573858
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-04-22 07:20:33 +0530

    Update lowest-common-ancestor-of-a-binary-search-tree.cpp

--- Trees/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ Trees/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,6 +10,17 @@
 
 class Solution {
 public:
+    // Iterative Approach : Time = O(H)  Space = O(1)
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(root) {
+            if(root->val > p->val && root->val > q->val) root = root->left;
+            else if(root->val < p->val && root->val < q->val) root = root->right;
+            else return root;
+        }
+        return nullptr;
+    }
+
+    // Recursive Approach : Time = O(H)  Space = O(H)
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(!root) return nullptr;
 

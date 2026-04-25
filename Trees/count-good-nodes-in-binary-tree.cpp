b506fd77ac98566a6fe90a5ac05e9b4d53a33d76
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-04-25 07:04:53 +0530

    Update count-good-nodes-in-binary-tree.cpp

--- Trees/count-good-nodes-in-binary-tree.cpp
+++ Trees/count-good-nodes-in-binary-tree.cpp
@@ -1,19 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void traverseTree(TreeNode* root, int& goodNodes, int maxValue) {
-        if(!root) return;
+    // void traverseTree(TreeNode* root, int& goodNodes, int maxValue) {
+    //     if(!root) return;
 
-        if(root->val >= maxValue) goodNodes++;
+    //     if(root->val >= maxValue) goodNodes++;
+    //     maxValue = max(maxValue, root->val);
+    //     traverseTree(root->left, goodNodes, maxValue);
+    //     traverseTree(root->right, goodNodes, maxValue);
+    // }
+
+    int getGoodNodes(TreeNode* root, int maxValue) {
+        if(!root) return 0;
+
         maxValue = max(maxValue, root->val);
-        traverseTree(root->left, goodNodes, maxValue);
-        traverseTree(root->right, goodNodes, maxValue);
+        return (root->val >= maxValue) + getGoodNodes(root->left, maxValue) + getGoodNodes(root->right, maxValue);
     }
 
     int goodNodes(TreeNode* root) {
         if(!root) return 0;
-
-        int goodNodes = 0;
-        traverseTree(root, goodNodes, root->val);
-        return goodNodes;
+        return getGoodNodes(root, root->val);
     }
 };

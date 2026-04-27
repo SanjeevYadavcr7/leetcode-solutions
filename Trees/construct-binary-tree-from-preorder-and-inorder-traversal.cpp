251f57b3058fe439295acc23fb7663b282b5da2a
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-04-27 23:53:38 +0530

    Create construct-binary-tree-from-preorder-and-inorder-traversal.cpp

---
+++ Trees/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTreeHelper(vector<int>& preorder, unordered_map<int, int>& inorderMap, int& idx, int start, int end) {
+        if(idx >= preorder.size() || start > end) return NULL;
+        
+        int rootVal = preorder[idx++];
+        int midVal = inorderMap[rootVal];
+        TreeNode* root = new TreeNode(rootVal);
+        root->left = buildTreeHelper(preorder, inorderMap, idx, start, midVal-1);
+        root->right = buildTreeHelper(preorder, inorderMap, idx, midVal+1, end);
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int, int> inorderMap;
+
+        for(int i = 0; i < inorder.size(); i++) {
+            inorderMap[inorder[i]] = i;
+        }
+        int idx = 0;
+        return buildTreeHelper(preorder, inorderMap, idx, 0, preorder.size()-1);
+    }
+};

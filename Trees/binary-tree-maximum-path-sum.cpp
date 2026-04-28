48b4f57bb4abe1545fbbf48c52e56066e9f1cae5
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-04-28 21:57:09 +0530

    Update binary-tree-maximum-path-sum.cpp

--- Trees/binary-tree-maximum-path-sum.cpp
+++ Trees/binary-tree-maximum-path-sum.cpp
@@ -1,1 +1,36 @@
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    // Time: O(N) | Space: O(H)
+    int maxPathSumHelper(TreeNode* root, int& maxSum) {
+        if(!root) return 0;
+
+        int leftSum = max(0, maxPathSumHelper(root->left, maxSum));
+        int rightSum = max(0, maxPathSumHelper(root->right, maxSum));
+
+        int currNodeSum = root->val + leftSum + rightSum;
+        maxSum = max(currNodeSum, maxSum);
+
+        return root->val + max(leftSum, rightSum);
+    }
+    
+    int maxPathSum(TreeNode* root) {
+        if(!root) return 0;
+
+        int maxSum = INT_MIN;
+        maxPathSumHelper(root, maxSum);
+        return maxSum;
+    }
+};

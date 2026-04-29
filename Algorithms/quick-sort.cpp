8790ce45ccdb774240f6ecf62a70b0450e635a8d
Sanjeev Yadav <sanjeevyadavcr7@gmail.com>
2026-04-30 00:02:11 +0530

    algorithm: quick-sort

--- Algorithms/quick-sort.cpp
+++ Algorithms/quick-sort.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+using namespace std;
 
+/*
+    Time Complexity: 
+    1. Best = O(N.LogN)
+    2. Average = O(N.LogN)
+    3. Worst = O(N^2)
+
+    Space Complexity: 
+    1. Worst = O(N) // if tree is skewed means array is sorted and we are selecting last element as pivot
+    2. Best or Average = O(LogN) // if tree is balanaced
+
+*/
+
+int getPivot(vector<int>& nums, int low, int high) {
+    int pivot = nums[high];
+    int i = low - 1;
+    for(int j = low; j < high; j++) {
+        if(nums[j] < pivot) {
+            i++;
+            swap(nums[i], nums[j]);
+        }
+    }
+    swap(nums[i+1], nums[high]);
+    return (i+1);
+}
+
+void quickSort(vector<int>& nums, int low, int high) {
+    if(low < high) {
+        int pivot = getPivot(nums, low, high);
+        quickSort(nums, low, pivot - 1);
+        quickSort(nums, pivot + 1, high);
+    }
+}

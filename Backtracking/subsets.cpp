fda5356a3a5f0f57cf8ab6e95a2795e4cb2be421
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-05-06 09:03:25 +0530

    Update subsets.cpp

--- Backtracking/subsets.cpp
+++ Backtracking/subsets.cpp
@@ -1,43 +1,48 @@
 class Solution {
 public:
     // Approach 1:
-    // void generateSubsets(vector<int>& nums, int idx, vector<int> temp, vector<vector<int>>& result) {
-    //     if(idx == nums.size()) return;
+    void generateSubsets(vector<int>& nums, int idx, vector<int>& temp, vector<vector<int>>& result) {
+        if(idx == nums.size()) return;
 
-    //     temp.push_back(nums[idx]);
-    //     result.push_back(temp);
-    //     for(int i = idx + 1; i < nums.size(); i++) {
-    //         generateSubsets(nums, i, temp, result);
-    //     }
-    // }
+        temp.push_back(nums[idx]);
+        result.push_back(temp);
+        for(int i = idx + 1; i < nums.size(); i++) {
+            generateSubsets(nums, i, temp, result);
+        }
+    }
 
-    // vector<vector<int>> subsets(vector<int>& nums) {
-    //     vector<vector<int>> result = {{}};
-    //     for(int i = 0; i < nums.size(); i++) {
-    //         vector<int> temp;
-    //         generateSubsets(nums, i, temp, result);
-    //     }
-    //     return result;
-    // }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result = {{}};
+        for(int i = 0; i < nums.size(); i++) {
+            vector<int> temp;
+            generateSubsets(nums, i, temp, result);
+        }
+        return result;
+    }
 
 
     // Approach 2:
-    void generateSubsets(vector<int>& nums, int idx, vector<int> subset, vector<vector<int>>& result) {
+    void generateSubsets(vector<int>& nums, int idx, vector<int>& subset, vector<vector<int>>& result) {
         if(idx == nums.size()) {
             result.push_back(subset);
             return;
         }
 
+        // Decision 1: Pick the current element
         subset.push_back(nums[idx]);
-        generateSubsets(nums, idx+1, subset, result); // pick the current element
+        generateSubsets(nums, idx+1, subset, result);
+
+        // BACKTRACK: Undo the decision before exploring the other branch
         subset.pop_back();
 
-        generateSubsets(nums, idx+1, subset, result); // do not pick the current element
+        // Decision 2: Do NOT pick the current element
+        generateSubsets(nums, idx+1, subset, result);
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> result;
-        generateSubsets(nums, 0, {}, result);
+        vector<int> subset;
+        generateSubsets(nums, 0, subset, result);
         return result;
     }
 };

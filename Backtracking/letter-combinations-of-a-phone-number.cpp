fd1c0b0077f14fa70b9600b2b7bf1997b13b1b57
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-05-08 08:20:00 +0530

    Add solution for letter combinations of phone number
    Implemented letter combinations for phone number digits using backtracking.

--- Backtracking/letter-combinations-of-a-phone-number.cpp
+++ Backtracking/letter-combinations-of-a-phone-number.cpp
@@ -1,1 +1,41 @@
 
+class Solution {
+public:
+    /*
+       Time Complexity: O(3^N.4^M), 
+       where N is the number of digits that map to 3 letters (like 2, 3, 4, 5, 6, 8)
+       M is the number of digits that map to 4 letters (7, 9).
+       
+       Space Complexity: O(N+M) for the recursion stack.
+    */
+
+    void initializeMap(vector<string>& numMap) {
+        numMap = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    }
+
+    void getLetterCombination(int idx, string& digits, string& current, vector<string>& result, vector<string>& numMap) {
+        if(idx == digits.size()) {
+            result.push_back(current);
+            return;
+        }
+        
+        string letters = numMap[digits[idx] - '0'];
+        for(char ch : letters) {
+            current.push_back(ch);
+            getLetterCombination(idx+1, digits, current, result, numMap);
+            current.pop_back();
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty()) return {};
+
+        vector<string> result;
+        string current = "";
+        vector<string> numMap;
+
+        initializeMap(numMap);
+        getLetterCombination(0, digits, current, result, numMap);
+        return result;
+    }
+};

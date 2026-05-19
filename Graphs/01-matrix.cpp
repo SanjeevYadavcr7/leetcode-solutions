1c0922ba56d338ac2d8761914146d47fd03c06aa
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-05-19 22:33:17 +0530

    Update 01-matrix.cpp

--- Graphs/01-matrix.cpp
+++ Graphs/01-matrix.cpp
@@ -1,1 +1,36 @@
 
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int rows = mat.size();
+        int cols = mat[0].size();
+        queue<pair<int, int>> nodes;
+        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(mat[i][j] == 0) {
+                    nodes.push({i, j}); // Push nodes to queue for Multi-source BFS
+                } else {
+                    mat[i][j] = -1;
+                }
+            }
+        }
+
+        while(!nodes.empty()) {
+            auto [x, y] = nodes.front();
+            nodes.pop();
+
+            for(auto [u, v] : directions) {
+                int new_x = x + u;
+                int new_y = y + v;
+                if(new_x >= 0 && new_x < rows && new_y >= 0 && new_y < cols && mat[new_x][new_y] == -1) {
+                    mat[new_x][new_y] = mat[x][y] + 1;
+                    nodes.push({new_x, new_y});
+                }
+            }
+        }
+
+        return mat;
+    }
+};

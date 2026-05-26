21b2f15efd1d4b2dc992678fac6207fcbcd57841
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-05-26 22:56:29 +0530

    Update word-ladder.cpp

--- Graphs/word-ladder.cpp
+++ Graphs/word-ladder.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    /*
+    Time Complexity: $O(M^2 * N), where M is the length of each word and N is the total number of words in the input list. 
+                     For each word popped from the queue, we iterate $M$ times, and inside that loop, we create a new string 
+                     copy which takes O(M) time to modify/hash.
+                     
+    Space Complexity: O(M.N) to hold the string states inside our hash set data structure and the underlying BFS memory footprint queue.
+    */
+
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set<string> visited(wordList.begin(), wordList.end());
 

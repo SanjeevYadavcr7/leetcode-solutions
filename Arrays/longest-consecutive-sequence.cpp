16c50ba67e0520a5eaa675d01946e35eb8d41d57
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-06-24 23:15:56 +0530

    Update longest-consecutive-sequence.cpp

--- Arrays/longest-consecutive-sequence.cpp
+++ Arrays/longest-consecutive-sequence.cpp
@@ -1,3 +1,12 @@
+/*
+    Time: O(N)  
+    Space: O(N)
+
+    IMPORTANT:
+    The key optimization is only starting a count if num - 1 is not present,
+    this ensures each element is processed in linear time O(N) else solution will be of O(N^2)
+*/
+
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {

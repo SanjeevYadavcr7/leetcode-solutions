eff3fc02efaac34189020f45153a522e4ffd0ebf
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-06-25 22:18:41 +0530

    Update number-of-islands.cpp

--- Graphs/number-of-islands.cpp
+++ Graphs/number-of-islands.cpp
@@ -5,38 +5,40 @@ Space: O(M.N)
 
 class Solution {
 private:
-    int m, n;
     vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-    bool isInsideGrid(int x, int y) {
-        return (x >= 0 && x < m && y >= 0 && y < n);
+    bool isVaildCell(int r, int c, int ROWS, int COLS) {
+        return (r >= 0 && c >= 0 && r < ROWS && c < COLS);
     }
 
-    void markNeighbourLands(int i, int j, vector<vector<char>>& grid) {
-        grid[i][j] = '0';
+    void markIsland(int r, int c, int ROWS, int COLS, vector<vector<char>>& grid) {
+        // Base case: out of bounds or water/visited
+        if(!isVaildCell(r, c, ROWS, COLS) || grid[r][c] == '0') {
+            return;
+        }
 
-        for(auto& [x, y] : directions) {
-            int new_x = x + i;
-            int new_y = y + j;
-            if(isInsideGrid(new_x, new_y) && grid[new_x][new_y] == '1') {
-                markNeighbourLands(new_x, new_y, grid);
-            }
+        // Mark as visited inside the function instance
+        grid[r][c] = '0';
+
+        for(auto[x, y] : directions) {
+            markIsland(x + r, y + c, ROWS, COLS, grid);
         }
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
-        int islandCount = 0;
+        int m = grid.size();
+        int n = grid[0].size();
         
+        int islands = 0;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(grid[i][j] == '1') {
-                    markNeighbourLands(i, j, grid);
-                    islandCount++;
+                    islands++;
+                    markIsland(i, j, m, n, grid);
                 }
             }
         }
-        return islandCount;
+
+        return islands;
     }
 };

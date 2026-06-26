6f06eddc8e1c803553f6dbfa75254f2df09abe70
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-06-26 23:39:38 +0530

    Create pacific-atlantic-water-flow.cpp

---
+++ Graphs/pacific-atlantic-water-flow.cpp
@@ -0,0 +1,49 @@
+class Solution {
+    vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
+    bool isValidCell(int r, int c, int ROWS, int COLS) {
+        return (r >= 0 && r < ROWS && c >= 0 && c < COLS);
+    }
+
+    void dfs(int r, int c, int ROWS, int COLS, int prevHeight, vector<vector<bool>>& visited, vector<vector<int>>& heights) {
+        if(!isValidCell(r, c, ROWS, COLS) || visited[r][c] || heights[r][c] < prevHeight) return;
+
+        visited[r][c] = true;
+
+        for(auto[x, y] : directions) {
+            dfs(r + x, c + y, ROWS, COLS, heights[r][c], visited, heights);
+        }
+    }
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int ROWS = heights.size();
+        int COLS = heights[0].size();
+
+        if(ROWS == 0 || COLS == 0) return {};
+
+        vector<vector<bool>> pacific(ROWS, vector<bool>(COLS, false));
+        vector<vector<bool>> atlantic(ROWS, vector<bool>(COLS, false));
+
+        // Flow water from top(pacific) and bottom(atlantic)
+        for(int c = 0; c < COLS; c++) {
+            dfs(0, c, ROWS, COLS, heights[0][c], pacific, heights);
+            dfs(ROWS - 1, c, ROWS, COLS, heights[ROWS - 1][c], atlantic, heights);
+        }
+
+        for(int r = 0; r < ROWS; r++) {
+            dfs(r, 0, ROWS, COLS, heights[r][0], pacific, heights);
+            dfs(r, COLS - 1, ROWS, COLS, heights[r][COLS - 1], atlantic, heights);
+        }
+
+        vector<vector<int>> result;
+        for(int r = 0; r < ROWS; r++) {
+            for(int c = 0; c < COLS; c++) {
+                if(pacific[r][c] && atlantic[r][c]) {
+                    result.push_back({r, c});
+                }
+            }
+        }
+
+        return result;
+    }
+};

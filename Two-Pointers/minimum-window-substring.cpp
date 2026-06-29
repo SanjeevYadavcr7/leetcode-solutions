74d8866c709ff842402549c7406d84ad7753e61a
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-06-29 23:20:58 +0530

    Update minimum-window-substring.cpp

--- Two-Pointers/minimum-window-substring.cpp
+++ Two-Pointers/minimum-window-substring.cpp
@@ -1,1 +1,33 @@
 
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if(t.empty()) return "";
+
+        unordered_map<char, int> countT, window;
+        for(char& ch : t) countT[ch]++;
+
+        int have = 0, need = countT.size(); 
+        int res_start = 0, min_len = INT_MAX;
+        int left = 0;
+        for(int right = 0; right < s.size(); right++) {
+            char ch = s[right];
+            window[ch]++;
+
+            if(countT.count(ch) && window[ch] == countT[ch]) have++;
+
+            while(have == need) {
+                int curr_len = right - left + 1;
+                if(curr_len < min_len) {
+                    min_len = curr_len;
+                    res_start = left;
+                }
+                
+                window[s[left]]--;
+                if(countT.count(s[left]) && window[s[left]] < countT[s[left]]) have--;
+                left++;
+            }
+        }
+        return min_len == INT_MAX ? "" : s.substr(res_start, min_len);
+    }
+};

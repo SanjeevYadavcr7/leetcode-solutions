2ef5f150023f344ee263a91cb3c91b5b101ca32e
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-07-02 23:42:30 +0530

    Update graph-valid-tree.cpp

--- Graphs/graph-valid-tree.cpp
+++ Graphs/graph-valid-tree.cpp
@@ -1,3 +1,20 @@
+/*
+    Approach: DFS
+    Time: O(N)
+    Actual time complexity is O(N + E), where N = no. of nodes and E = no. of edges
+    But in this question as E ~ V - 1 because it is a graph hence constant part in complexity 
+    becomes negligible and overall time complexity is O(N + N) = O(2.N) = O(N)
+
+    This is fine when N is small, but if N is large then it causes problem.
+    Why?
+    Because there are two phases in this approach:
+    1. Build graph which takes O(N + E) ~ O(N)
+    2. Actual DFS to find cycle, which again takes O(N + E) ~ O(N)
+
+    Now, the overall time complexity will be still O(N) but the constant which is getting ignored
+    is HUGGGGGGGEEEEEE. This is why this approach is not optimal solution and we use DSU as optimal
+    solution for this question.
+*/
 
 class Solution {
 private:

c389400b0e2aad32786abd8af30f29c54c97a7fe
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-07-02 23:14:09 +0530

    Update graph-valid-tree.cpp

--- Graphs/graph-valid-tree.cpp
+++ Graphs/graph-valid-tree.cpp
@@ -1,1 +1,32 @@
 
+class Solution {
+private:
+    void dfs(int node, unordered_set<int>& visited, vector<vector<int>>& graph) {
+        if(visited.find(node) != visited.end()) return;
+
+        visited.insert(node);
+
+        for(int& nbr : graph[node]) {
+            dfs(nbr, visited, graph);
+        }
+    }
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (edges.size() != n - 1) return false;
+
+        vector<vector<int>> graph(n);
+
+        for(auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+
+        unordered_set<int> visited;
+
+        dfs(0, visited, graph);
+
+        return visited.size() == n;
+    }
+};

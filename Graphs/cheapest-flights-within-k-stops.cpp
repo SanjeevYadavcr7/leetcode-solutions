f8f70bb4311c6fda9024b7f164f77f77fdc6bd1f
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-07-10 09:07:18 +0530

    Update cheapest-flights-within-k-stops.cpp

--- Graphs/cheapest-flights-within-k-stops.cpp
+++ Graphs/cheapest-flights-within-k-stops.cpp
@@ -1,4 +1,40 @@
 /*
+    Algorithm: Bellman-Ford (Custom)
+    Time: O(K.E)
+    Space: O(N)
+*/
+
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
+
+        // Relax edges at most k+1 times (k stops = k+1 edges)
+        for(int i = 0; i <= k; i++) {
+            /* 
+                Temp array makes sure only level wise-relaxation happens.
+                Use a copy so updates within this round don't cascade
+                each round should only add ONE more edge to any path
+            */
+            vector<int> temp = dist;
+
+            for(auto& flight : flights) {
+                int src = flight[0], dst = flight[1], price = flight[2];
+                if(dist[src] != INT_MAX && dist[src] + price < temp[dst]) {
+                    temp[dst] = dist[src] + price;
+                }
+            }
+
+            dist = temp;
+        }
+
+        return dist[dst] == INT_MAX ? -1 : dist[dst];
+    }
+};
+
+
+/* ------------------------------------------------------------------------------------
     Total Work = Maximum Stops * Work per stop = (K + 1) * E
     
     Time: O(K.E)

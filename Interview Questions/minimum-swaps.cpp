f5023c60b136d6c82d7c22c9dadae6707cf414df
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-07-12 00:18:27 +0530

    Update minimum-swaps.cpp

--- Interview Questions/minimum-swaps.cpp
+++ Interview Questions/minimum-swaps.cpp
@@ -1,1 +1,36 @@
-// Target - July 2026
+/* 
+July 2026
+Question Link: https://www.geeksforgeeks.org/problems/minimum-swaps/1
+*/
+class Solution {
+  public:
+    int minSwaps(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> sortedArr = arr;
+        unordered_map<int, int> numIndexMap;
+        
+        // Maintaining sorted array to get original position of elements
+        sort(sortedArr.begin(), sortedArr.end());
+        
+        for(int i = 0; i < n; i++) {
+            numIndexMap[arr[i]] = i;
+        }
+        
+        int minSwaps = 0;
+        for(int i = 0; i < n; i++) {
+            // if mismatch -> then we need to re-position element in original array
+            if(arr[i] != sortedArr[i]) {
+                minSwaps++;
+                
+                int sortedNumIdx = numIndexMap[sortedArr[i]];
+                
+                swap(arr[i], arr[sortedNumIdx]);
+                
+                numIndexMap[arr[sortedNumIdx]] = sortedNumIdx;
+                numIndexMap[arr[i]] = i;
+            }
+        }
+        
+        return minSwaps;
+    }
+};

93fbb01f20d76f4fb9725e8f736fdcf7501f4781
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-07-22 00:00:28 +0530

    Update frog-jump.cpp

--- DP/frog-jump.cpp
+++ DP/frog-jump.cpp
@@ -1,3 +1,32 @@
+// General Solution for K steps:
+
+class Solution {
+public:
+    int frogJump(vector<int>& heights, int k) {
+        int n = heights.size();
+        vector<int> cost(n, INT_MAX);
+
+        cost[0] = 0;
+        cost[1] = abs(heights[0] - heights[1]);
+
+        for(int i = 2; i < n; i++) {
+            int minCost = INT_MAX;
+            for(int j = 1; j <= k; j++) {
+                int currJumpCost = abs(heights[i] - heights[i-j]) + cost[i-j];
+                minCost = min(minCost, currJumpCost);
+            }
+            cost[i] = minCost;
+        }
+
+        return cost[n - 1];
+    }
+};
+
+
+
+// --------------------------------------------------------------------------------------
+// For 2 steps(i+1, i+2)
+
 class Solution {
   public:
     int minCost(vector<int>& height) {

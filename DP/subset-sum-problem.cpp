4a8062040fc6b040eca7e5a65f69bf44c6cd34ec
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-08-04 22:36:36 +0530

    Update subset-sum-problem.cpp

--- DP/subset-sum-problem.cpp
+++ DP/subset-sum-problem.cpp
@@ -1,4 +1,31 @@
 /*
+Approach: Memoization
+
+class Solution {
+  private:
+    bool solve(int idx, int target, vector<vector<int>>& memo, vector<int>& arr) {
+        if(target == 0) return true;
+        
+        if(idx >= arr.size() || target < 0) return false;
+        
+        if(memo[idx][target] != -1) return memo[idx][target];
+        
+        bool take = solve(idx + 1, target - arr[idx], memo, arr);
+        bool skip = solve(idx + 1, target, memo, arr);
+        
+        return memo[idx][target] = (take || skip);
+    }
+  public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        vector<vector<int>> memo(n, vector<int>(sum + 1, -1));
+        
+        return solve(0, sum, memo, arr);
+    }
+};
+*/
+
+/*
 Approach: Recursion
 Time Complexity: O(2^N) :: Space Complexity: O(N)
 

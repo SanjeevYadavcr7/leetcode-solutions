c52ec82f705cb6cc05fe04d78f139800fe64c48f
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-08-15 05:44:00 +0530

    Update coin-change-ii.cpp

--- DP/coin-change-ii.cpp
+++ DP/coin-change-ii.cpp
@@ -1,3 +1,31 @@
+/*---------------------------------------------------------------
+Approach: DP (2D array)
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<unsigned int>> dp(n + 1, vector<unsigned int>(amount + 1, 0));
+
+        for(int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        for(int i = 1; i <= n; i++) {
+            int coin = coins[i - 1];
+            for(int j = 1; j <= amount; j++) {
+                dp[i][j] = dp[i - 1][j];
+
+                int take = 0;
+                if(j >= coin) {
+                    dp[i][j] += dp[i][j - coin];
+                }
+            }
+        }
+
+        return dp[n][amount];
+    }
+};
+*/
+
 /*
 Approach: Recursion + Memoization
 

1f6887516593d2ed230aefe63362459016f226b5
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-08-15 20:14:57 +0530

    Update knapsack-with-duplicate-item.cpp

--- DP/knapsack-with-duplicate-item.cpp
+++ DP/knapsack-with-duplicate-item.cpp
@@ -1,1 +1,28 @@
 
+/*----------------------------------------------------------------------------
+Approach: Recursion + Memoization
+
+class Solution {
+    int getMaxProfit(int idx, int cap, vector<int>& val, vector<int>& wt, vector<vector<int>>& memo) {
+        if(idx == val.size() || cap <= 0) return 0;
+        
+        if(memo[idx][cap] != -1) return memo[idx][cap];
+        
+        int skip = getMaxProfit(idx + 1, cap, val, wt, memo);
+        
+        int take = 0;
+        if(cap >= wt[idx]) {
+            take = val[idx] + getMaxProfit(idx, cap - wt[idx], val, wt, memo);   
+        }
+        
+        return memo[idx][cap] = max(skip, take);
+    }
+  public:
+    int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
+        int n = val.size();
+        vector<vector<int>> memo(n + 1, vector<int>(capacity + 1, -1));
+        
+        return getMaxProfit(0, capacity, val, wt, memo);
+    }
+};
+*/

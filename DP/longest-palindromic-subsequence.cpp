d7efeb4a703237bf5fe33c59a2c244bd6ead5273
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-09-09 22:38:19 +0530

    Create longest-palindromic-subsequence.cpp

---
+++ DP/longest-palindromic-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+
+        for(int gap = 0; gap < n; gap++) {
+            
+            for(int i = 0, j = gap; j < n; i++, j++) {
+                if(gap == 0) {
+                    dp[i][j] = 1;
+                } else if(gap == 1) {
+                    dp[i][j] = (s[i] == s[j]) ? 2 : 1;
+                } else {
+                    if(s[i] == s[j]) {
+                        dp[i][j] = 2 + dp[i + 1][j - 1];
+                    } else {
+                        dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+};

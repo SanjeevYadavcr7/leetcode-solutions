2d451d4ebc98e5403efa8cc4e70945324bd8c799
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2026-09-10 21:37:49 +0530

    Create distinct-subsequences.cpp

---
+++ DP/distinct-subsequences.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int M = s.size();
+        int N = t.size();
+
+        // if "s" is smaller than "t" then its subsequence cannot form "t"
+        if(M < N) return 0;
+
+        vector<vector<unsigned long long>> dp(M + 1, vector<unsigned long long>(N + 1));
+
+        // if "t" is empty and "s" is not then one subsequence(empty) is always possible
+        for(int i = 0; i <= M; i++) {
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1; i <= M; i++) {
+            char ch1 = s[i - 1];
+            for(int j = 1; j <= N; j++) {
+                char ch2 = t[j - 1];
+                if(ch1 == ch2) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[M][N];
+    }
+};
